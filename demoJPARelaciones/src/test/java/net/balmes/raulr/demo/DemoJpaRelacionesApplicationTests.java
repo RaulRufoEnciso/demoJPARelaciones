b3c12a09d053f94544fd637e4f2697f4f5b6c26c
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2022-11-07 15:47:34 +0100

    Primer

---
+++ demoJPARelaciones/src/test/java/net/balmes/raulr/demo/DemoJpaRelacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.balmes.raulr.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpaRelacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
